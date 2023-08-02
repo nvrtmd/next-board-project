4e4d4db88d599dd59ecb0aa178da9beb19cca79c
YUZAMIN <untmd.crmn@gmail.com>
2023-08-03 02:27:38 +0900

    fix: _app.tsx에서 theme을 import하는 방식을 수정하라

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from '../styles/global-style';
-import theme from '../styles/theme';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
