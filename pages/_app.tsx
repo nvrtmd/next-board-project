3e97b6d44edd2126ceb1182ad2a4d8aca0a92cb5
YUZAMIN <untmd.crmn@gmail.com>
2023-08-03 01:23:23 +0900

    feat: _app.tsx 파일을 생성하라
    - 모든 페이지에 공통으로 적용할 레이아웃을 설정하기 위함

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from '../styles/global-style';
+import { theme } from '../styles/theme';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
