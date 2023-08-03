2f4b13e232d8dd75db7fb76f29c9f97c1ede19c9
YUZAMIN <untmd.crmn@gmail.com>
2023-08-03 13:51:23 +0900

    fix: _document.tsx 파일의 export 방식을 변경하라

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const originalRenderPage = ctx.renderPage;
 
@@ -26,5 +26,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
