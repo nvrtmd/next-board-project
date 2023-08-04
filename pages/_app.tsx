7c9a5fe2431a94d68ac8271108a6d9d738d04590
YUZAMIN <untmd.crmn@gmail.com>
2023-08-04 14:28:41 +0900

    feat: react-query 사용을 위한 설정을 적용하라
    - _app.tsx에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
-
-import { GlobalStyle } from '../styles/global-style';
-import { theme } from '../styles/theme';
+import { GlobalStyle } from 'styles/global-style';
+import { theme } from 'styles/theme';
 import Layout from '@/components/layout/Layout';
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <ReactQueryDevtools />
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
