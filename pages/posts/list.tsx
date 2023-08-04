50a923490358e9935a4abecd3cbdd7b54a9f7b81
YUZAMIN <untmd.crmn@gmail.com>
2023-08-04 21:46:09 +0900

    feat: 게시글 목록에 무한스크롤 기능을 적용하라

--- pages/posts/list.tsx
+++ pages/posts/list.tsx
@@ -1,26 +1,66 @@
+import styled from 'styled-components';
 import { boardApi } from 'api/board';
 import { Post } from 'global/types';
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import PostItem from 'components/posts/PostItem';
+import { useEffect, useRef } from 'react';
+import useIntersectionObserver from 'hooks/useIntersectionObserver';
+interface LoadingProps {
+  isDisplayed?: boolean;
+}
 
 export default function PostListPage() {
-  const PAGE_SIZE = 3;
+  const PAGE_SIZE = 10;
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError, error } = useInfiniteScroll<Post>(
     ['postList'],
     (pageParam) => boardApi.getPostList(pageParam, PAGE_SIZE),
     PAGE_SIZE,
   );
+  const intersectRef = useRef<HTMLDivElement>(null);
+  const { isIntersect } = useIntersectionObserver(intersectRef, {
+    root: null,
+    rootMargin: '50px',
+    threshold: 0.01,
+  });
+
+  useEffect(() => {
+    if (isIntersect) {
+      fetchNextPage();
+    }
+  }, [isIntersect]);
 
   return (
-    <div>
-      {data?.map((post: Post) => (
-        <div key={post.post_idx}>
-          <h3>
-            {post.post_idx} {post.post_title}
-          </h3>
-        </div>
-      ))}
-
-      {hasNextPage && <div onClick={() => fetchNextPage()}>다음 게시글</div>}
-    </div>
+    <Wrapper>
+      <PostList>
+        {data?.map((post: Post) => (
+          <PostItem data={post} onClick={() => console.log(123)} key={post.post_idx} />
+        ))}
+      </PostList>
+      <VisibleCitiesLoading ref={intersectRef} isDisplayed={hasNextPage}>
+        LOADING...
+      </VisibleCitiesLoading>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  min-height: 100%;
+`;
+
+const PostList = styled.div`
+  width: 85%;
+  display: grid;
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const VisibleCitiesLoading = styled.div<LoadingProps>`
+  ${({ isDisplayed }) =>
+    !isDisplayed &&
+    `
+    display: none !important;
+  `}
+  text-align: center;
+  font-size: 1.4rem;
+  padding: 1rem 0;
+`;
