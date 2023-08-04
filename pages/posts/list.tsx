b2ff8519ebc5eb5d45d0856b94aeb634efb8d592
YUZAMIN <untmd.crmn@gmail.com>
2023-08-04 16:50:13 +0900

    fix: 기존 로직을 useInfiniteScroll hook으로 대체하라

--- pages/posts/list.tsx
+++ pages/posts/list.tsx
@@ -1,33 +1,24 @@
 import { boardApi } from 'api/board';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { Post } from '@/global/types';
+import { Post } from 'global/types';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
 export default function PostListPage() {
-  const PAGE_SIZE = 2;
-  const { data, fetchNextPage, hasNextPage, isError } = useInfiniteQuery(
+  const PAGE_SIZE = 3;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isError, error } = useInfiniteScroll<Post>(
     ['postList'],
-    ({ pageParam = 0 }) => boardApi.getPostList(pageParam, PAGE_SIZE),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        if (lastPage?.data?.data.length === PAGE_SIZE) {
-          return allPages.length * PAGE_SIZE;
-        }
-        return undefined;
-      },
-    },
+    (pageParam) => boardApi.getPostList(pageParam, PAGE_SIZE),
+    PAGE_SIZE,
   );
 
   return (
     <div>
-      {data?.pages.map((page) => {
-        return page?.data.data.map((post: Post) => (
-          <div key={post.post_idx}>
-            <h3>
-              {post.post_idx} {post.post_title}
-            </h3>
-          </div>
-        ));
-      })}
+      {data?.map((post: Post) => (
+        <div key={post.post_idx}>
+          <h3>
+            {post.post_idx} {post.post_title}
+          </h3>
+        </div>
+      ))}
 
       {hasNextPage && <div onClick={() => fetchNextPage()}>다음 게시글</div>}
     </div>
