053a42291460f00a7b012e5e4f94198e24be988a
YUZAMIN <untmd.crmn@gmail.com>
2023-08-04 16:07:16 +0900

    feat: useInfiniteQuery를 사용하여 게시글 목록을 fetch하라
    - 게시글 목록 페이지에서 게시글 목록 데이터를 부분적으로 가져오기 위함

--- pages/posts/list.tsx
+++ pages/posts/list.tsx
@@ -1,3 +1,35 @@
+import { boardApi } from 'api/board';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { Post } from '@/global/types';
+
 export default function PostListPage() {
-  return <div>Hello, This is post list page!</div>;
+  const PAGE_SIZE = 2;
+  const { data, fetchNextPage, hasNextPage, isError } = useInfiniteQuery(
+    ['postList'],
+    ({ pageParam = 0 }) => boardApi.getPostList(pageParam, PAGE_SIZE),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage?.data?.data.length === PAGE_SIZE) {
+          return allPages.length * PAGE_SIZE;
+        }
+        return undefined;
+      },
+    },
+  );
+
+  return (
+    <div>
+      {data?.pages.map((page) => {
+        return page?.data.data.map((post: Post) => (
+          <div key={post.post_idx}>
+            <h3>
+              {post.post_idx} {post.post_title}
+            </h3>
+          </div>
+        ));
+      })}
+
+      {hasNextPage && <div onClick={() => fetchNextPage()}>다음 게시글</div>}
+    </div>
+  );
 }
