41a7efd75b34c4b51659957771353acf0eb562a8
YUZAMIN <untmd.crmn@gmail.com>
2023-08-05 20:40:30 +0900

    feat: PostForm 컴포넌트를 구현하라
    - 게시글 작성을 위한 form 컴포넌트

---
+++ components/posts/PostForm.tsx
@@ -0,0 +1,81 @@
+import { useMemo } from 'react';
+import styled from 'styled-components';
+import Input from 'components/common/Input';
+import TextArea from 'components/common/TextArea';
+import Button from 'components/common/Button';
+
+interface PostFormProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  formTitle?: string;
+  title: string;
+  contents: string;
+  onTitleChange: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
+  onContentsChange: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
+  onCancelButtonClick: () => void;
+}
+
+function PostForm({
+  onSubmit,
+  formTitle,
+  title,
+  contents,
+  onTitleChange,
+  onContentsChange,
+  onCancelButtonClick,
+}: PostFormProps) {
+  const isSubmittable = () => {
+    return title.length > 0 && contents.length > 0;
+  };
+
+  const formHeader = useMemo(() => <FormTitle>{formTitle}</FormTitle>, []);
+
+  const formFooter = useMemo(
+    () => (
+      <ButtonWrapper>
+        <Button type="submit" isActivated={isSubmittable()} name="Submit" />
+        <Button type="button" isActivated={true} name="Cancel" onClick={onCancelButtonClick} />
+      </ButtonWrapper>
+    ),
+    [isSubmittable],
+  );
+  return (
+    <Form onSubmit={onSubmit}>
+      {formHeader}
+      <Input name="title" type="title" value={title} onChange={onTitleChange} placeholder="Write title" />
+      <ContentsWrapper>
+        <TextArea placeholder="Write contents" name="contents" onChange={onContentsChange} value={contents} />
+      </ContentsWrapper>
+      {formFooter}
+    </Form>
+  );
+}
+
+export default PostForm;
+
+const Form = styled.form`
+  width: 100%;
+  padding: 2rem 1.5rem;
+  height: 100%;
+`;
+
+const FormTitle = styled.h1`
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 30px;
+`;
+
+const ContentsWrapper = styled.div`
+  margin-top: 1.5rem;
+  height: 60%;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2.5rem;
+  width: 50%;
+  margin: 0 auto;
+  max-width: 300px;
+`;
