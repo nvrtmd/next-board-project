e30487e4c7750cef6ea258e4f8f0f95aa85400aa
YUZAMIN <untmd.crmn@gmail.com>
2023-08-07 14:24:41 +0900

    feat: 댓글 관련 인터페이스를 정의하라

--- global/types.ts
+++ global/types.ts
@@ -22,3 +22,21 @@ export interface CustomError {
   code: string;
   message: string;
 }
+
+export interface NewComment {
+  contents: string;
+}
+
+export interface Comment {
+  comment_contents: string;
+  comment_idx: number;
+  comment_writer: {
+    member_id: string;
+    member_nickname: string;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
+  member_idx?: number;
+  post_idx?: number;
+}
