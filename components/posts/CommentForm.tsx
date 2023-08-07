1ea64109129bd434d47e36eeec84d81e82f4e7de
YUZAMIN <untmd.crmn@gmail.com>
2023-08-07 14:23:58 +0900

    feat: CommentForm 컴포넌트를 구현하라

---
+++ components/posts/CommentForm.tsx
@@ -0,0 +1,81 @@
+import { useMemo } from 'react';
+import styled from 'styled-components';
+import TextArea from 'components/common/TextArea';
+import Button from 'components/common/Button';
+
+interface CommentFormProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  comment: string;
+  onCommentChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  formTitle?: string;
+  type: 'modify' | 'create';
+  onCancelButtonClick?: () => void;
+  isActivated?: boolean;
+}
+
+interface ButtonWrapperProps {
+  type: 'modify' | 'create';
+}
+
+export const CommentForm = ({
+  onSubmit,
+  onCommentChange,
+  comment,
+  formTitle,
+  type,
+  onCancelButtonClick,
+  isActivated,
+}: CommentFormProps) => {
+  const isSubmittable = comment.length > 0;
+
+  const formHeader = useMemo(() => formTitle && <FormTitle>{formTitle}</FormTitle>, []);
+
+  const formFooter = useMemo(
+    () => (
+      <ButtonWrapper type={type}>
+        <Button name={type === 'modify' ? 'Modify' : 'Write comment'} type="submit" isActivated={isSubmittable} />
+        {type === 'modify' && <Button name="Cancel" type="button" onClick={onCancelButtonClick} isActivated={true} />}
+      </ButtonWrapper>
+    ),
+    [isActivated],
+  );
+
+  return (
+    <Form onSubmit={onSubmit}>
+      {formHeader}
+      <CommentWrapper>
+        <TextArea placeholder="Write comment" name="comment" onChange={onCommentChange} value={comment} />
+      </CommentWrapper>
+      {formFooter}
+    </Form>
+  );
+};
+
+const Form = styled.form`
+  margin-bottom: 2.5rem;
+`;
+
+const FormTitle = styled.div`
+  display: flex;
+  font-size: 1.4rem;
+  margin-bottom: 0.5rem;
+`;
+
+const CommentWrapper = styled.div`
+  height: 100px;
+  margin-bottom: 0.8rem;
+`;
+
+const ButtonWrapper = styled.div<ButtonWrapperProps>`
+  ${({ type }) =>
+    type === 'modify' &&
+    `    
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+  `}
+  padding: 3rem;
+  width: 50%;
+  margin: 0 auto;
+  max-width: 300px;
+`;
