6af49d35c052de01f02abc687efff4da9ee1699e
YUZAMIN <untmd.crmn@gmail.com>
2023-08-08 23:55:59 +0900

    feat: AboutPage를 구성하라

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,30 @@
+import styled from 'styled-components';
+
 export default function AboutPage() {
-  return <div>Hello, This is About page!</div>;
+  return (
+    <Wrapper>
+      <Title>This is About page</Title>
+      <Subtitle>Nice to meet you!</Subtitle>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  min-height: 100%;
+  text-align: center;
+  line-height: 1.5;
+  padding: 50px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-weight: 900;
+  font-size: 2rem;
+`;
+
+const Subtitle = styled.h2`
+  font-weight: 700;
+  font-size: 1.8rem;
+`;
