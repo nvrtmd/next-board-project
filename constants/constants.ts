8efefa1d79b55678ce939c78f06db3fa63a9da8a
YUZAMIN <untmd.crmn@gmail.com>
2023-08-09 00:38:44 +0900

    feat: 게시판 관련 에러 메세지를 상수화하라

---
+++ constants/constants.ts
@@ -0,0 +1,12 @@
+export const BOARD_ERROR_MESSAGE = {
+  CANNOT_GET_POST_LIST_FROM_SERVER: '서버로부터 게시글 목록을 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_GET_POST_DATA_FROM_SERVER: '서버로부터 게시글 정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_GET_COMMENT_LIST: '서버로부터 댓글 목록을 불러오지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_CREATE_POST: '게시글 작성에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_MODIFY_OR_DELETE_POST: '게시글 수정 및 삭제에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_MODIFY_POST: '게시글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_DELETE_POST: '게시글을 삭제하지 못했습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_CREATE_COMMENT: '댓글 작성에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_MODIFY_COMMENT: '댓글 수정에 실패하였습니다. 잠시 후 다시 시도해주세요.',
+  CANNOT_DELETE_COMMENT: '댓글을 삭제하지 못했습니다. 잠시 후 다시 시도해주세요.',
+};
